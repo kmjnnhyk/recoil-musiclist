df3ffd0c951f7495b05564b81c5cbdbcff879b93
kmjnnhyk <rlawlsgur620@gmail.com>
2022-01-27 23:33:41 +0900

    resize event listenr 추가 및 resize 모니터링 주기 관리를 위한 lodash 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { debounce } from 'lodash';
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import Home from './pages/Home';
 import RecomendationPage from './pages/RecomendationPage';
+import { windowSizeState } from './store/states';
 
 function App() {
+  const setWindowSize = useSetRecoilState(windowSizeState);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleResize = debounce(() => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, 200);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
