aa3d73596d516e9f835a3d31e908085a521845b8
kmjnnhyk <rlawlsgur620@gmail.com>
2022-02-09 00:04:34 +0900

    addToBasket, deleteFromBasket 함수 정리

--- src/store/basketDispatcher.js
+++ src/store/basketDispatcher.js
@@ -2,36 +2,30 @@ import { useRecoilCallback } from 'recoil';
 import { musicBasketState } from './states';
 
 export const createBasketDispatcher = () => {
-  let isDuplication = false;
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  // const addToBasket = useRecoilCallback(({ snapshot, set }) => (item) => {
+  //   const prevItems = snapshot.getLoadable(musicBasketState);
+  //   const newItem = {
+  //     id: item.id,
+  //     title: item.title,
+  //   };
+  //   console.log(prevItems);
+  //   set(musicBasketState, (oldItems) => [...oldItems, newItem]);
+  // });
+
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const addToBasket = useRecoilCallback(({ set }) => (item) => {
-    const newItem = {
-      id: item.id,
-      title: item.title,
-    };
-    //set(musicBasketState, (oldItems) => [...oldItems, newItem]);
-    set(musicBasketState, (oldItems) => {
-      oldItems.map((olditem) => {
-        if (olditem.id === newItem.id) {
-          isDuplication = true;
-          // eslint-disable-next-line array-callback-return
-          return;
-        }
-        return [...new Set([...oldItems, newItem])];
-      });
-    });
+    set(musicBasketState, (oldItems) => [...oldItems, item]);
   });
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const deleteFromBasket = useRecoilCallback(({ set }) => (id) => {
     set(musicBasketState, (oldItems) => {
-      const newItems = oldItems.filter((oldItem) => oldItem.id !== id);
-      return newItems;
+      return oldItems.filter((oldItem) => oldItem.id !== id);
     });
   });
   return {
     addToBasket,
     deleteFromBasket,
-    isDuplication,
   };
 };
